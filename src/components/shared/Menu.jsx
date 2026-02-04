468a1b8080a8302727b22d25cfb7c5695fb59d95
Milton Mamani <miltin.mamani010@gmail.com>
2026-02-04 14:43:47 -0300

    agrege en logo del carrito pero falta definir bien si sera ese o no estoy trabajando sobre ese logo

--- src/components/shared/Menu.jsx
+++ src/components/shared/Menu.jsx
@@ -73,6 +73,7 @@ const Menu = ({ usuarioAdmin, setUsuarioAdmin }) => {
                 Solicitar Turno
               </Nav.Link>
               {/* logo del carrito */}
+              {/* falta definir el logo del carrito si sera una imagen o un carrito o solo el nombre del carrito */}
               <Nav.Link
                 as={NavLink}
                 to="/detalle-producto/1"
